1de0f396efde322b680488a92e84f9a78b41c223
Jon Roes <jon.roes@snowflake.com>
2022-12-15 16:34:22 -0500

    license headers

--- headless_render.py
+++ headless_render.py
@@ -1,17 +1,27 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Any, List, Optional
-from streamlit.proto.ForwardMsg_pb2 import ForwardMsg
+
 from streamlit.proto.ClientState_pb2 import ClientState
 from streamlit.proto.Delta_pb2 import Delta
 from streamlit.proto.Element_pb2 import Element
-
+from streamlit.proto.ForwardMsg_pb2 import ForwardMsg
 from streamlit.runtime import Runtime, RuntimeConfig
-from streamlit.runtime.memory_media_file_storage import MemoryMediaFileStorage
 from streamlit.runtime.forward_msg_queue import ForwardMsgQueue
-from streamlit.runtime.scriptrunner import (
-    RerunData,
-    ScriptRunner,
-    ScriptRunnerEvent,
-)
+from streamlit.runtime.memory_media_file_storage import MemoryMediaFileStorage
+from streamlit.runtime.scriptrunner import RerunData, ScriptRunner, ScriptRunnerEvent
 from streamlit.runtime.state.session_state import SessionState
 from streamlit.runtime.uploaded_file_manager import UploadedFileManager
 
@@ -119,27 +129,29 @@ class HeadlessScriptRunner(ScriptRunner):
 
 
 def run():
-    config = RuntimeConfig('test.py', "", MemoryMediaFileStorage(""))
+    config = RuntimeConfig("test.py", "", MemoryMediaFileStorage(""))
     _ = Runtime(config)
 
-    runner = HeadlessScriptRunner('test.py')
+    runner = HeadlessScriptRunner("test.py")
     runner.start()
     runner.join()
     print(runner.widgets)
 
-    runner = HeadlessScriptRunner('test.py')
-    runner.set_widget('name', 'jon')
+    runner = HeadlessScriptRunner("test.py")
+    runner.set_widget("name", "jon")
     runner.start()
     runner.join()
-    assert runner.widgets.get('name') == 'jon'
-    import pdb; pdb.set_trace()
+    assert runner.widgets.get("name") == "jon"
+    import pdb
+
+    pdb.set_trace()
     print(runner.widgets)
 
-    runner = HeadlessScriptRunner('test.py')
-    runner.set_widget('name', 'will')
+    runner = HeadlessScriptRunner("test.py")
+    runner.set_widget("name", "will")
     runner.start()
     runner.join()
-    assert runner.widgets.get('name') == 'will'
+    assert runner.widgets.get("name") == "will"
     print(runner.widgets)
 
 
